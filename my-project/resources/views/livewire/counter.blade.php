d319cfc067e0e78270f19a0938decc389a636193
MargaridaMaria <margarida.rodrigues@escolar.ifrn.edu.br>
2023-07-19 12:09:17 -0300

    trabalho

---
+++ my-project/resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+<h1>Oi</h1>
+
+<button wire:click="increment">+</button>
+<h1>{{ $count }}</h1>
+    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+</div>
